2d22fb775b0fc78ff99a81ff06cd11754c3d2a63
Vikas1798 <hkvikas1798@gmail.com>
2024-12-09 11:47:05 +0530

    tailwind config file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-                extend: {
-                        colors: {
-                            // prime: "#FFFFFF",
-                        },
-                },
-    },
-};
+export default {
+        content: ["./src/**/*.{js,ts,jsx,tsx}"],
+        theme: {
+            extend: {},
+        },
+        plugins: [],
+    };
